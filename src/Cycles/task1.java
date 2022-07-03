8ea2c1d9c2418b3ad525495ddf955eeb69d612b8
TimurSafin27 <108293724+TimurSafin27@users.noreply.github.com>
2022-07-03 17:07:13 +0500

    2

---
+++ src/Cycles/task1.java
@@ -0,0 +1,24 @@
+package Cycles;
+
+public class task1
+{
+    public static void main (String[] args) //Объявить переменные всех вам известных типов и вывести их значения на экран.
+    {
+        boolean bo1 = true;
+        byte by1=127;
+        short sh1=32767;
+        int in1=2147483647;
+        long lo1=1234567890;
+        float f1=123.456f;
+        double do1=123.456;
+        char ch1='h';
+        System.out.println("boolean - "+bo1);
+        System.out.println("byte - " + by1);
+        System.out.println("short - "+sh1);
+        System.out.println("int - "+in1);
+        System.out.println("long - "+lo1);
+        System.out.println("float - "+f1);
+        System.out.println("double - "+do1);
+        System.out.println("char - "+ch1);
+    }
+}
---
+++ src/Cycles/task2.java
@@ -0,0 +1,26 @@
+package Cycles;
+
+public class task2
+        /*Объявить строку с вашим полным именем и вывести на экран результат работы
+        всех стандартных методов работы со строками*/
+{
+    public static void main (String[] args)
+    {
+        String MyName="Сафин Тимур Зиганурович";
+        String MyName1="САФИН ТИМУР ЗИГАНУРОВИЧ";
+        char[] Surname= new char[5];
+        System.out.println("длина - " + MyName.length());
+        System.out.println("пустая? - " + MyName.isEmpty());
+        System.out.println("Инициалы - " + MyName.charAt(0) + MyName.charAt(6) + MyName.charAt(12));
+        MyName.getChars(0, 5, Surname, 0);
+        String Surname2=String.valueOf(Surname);
+        System.out.println("Фамилия - " + Surname2);
+        System.out.println("Равны? - " + MyName.equals(MyName1));
+        System.out.println("Равны независимо от регистра? - " + MyName.equalsIgnoreCase(MyName1));
+        System.out.println("startsWith - " + MyName.startsWith(Surname2));
+        System.out.println("startsWith - " + MyName.endsWith(Surname2));
+        String Name1=" Тимур";
+        System.out.println("Фамилия+имя - " + Surname2.concat(Name1));
+    }
+}
+
