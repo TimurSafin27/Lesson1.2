91fa788b39b2f3f3728856e3bf0c8cccc687c8b3
TimurSafin27 <108293724+TimurSafin27@users.noreply.github.com>
2022-07-03 23:45:28 +0500

    1

--- src/Cycles/task4.java
+++ src/Cycles/task4.java
@@ -6,7 +6,7 @@ public class task4
 {
     public static void main (String[] args)
     {
-        System.out.println("Цикл while");
+        System.out.println("Цикл while:");
         byte month=0;
         int zp=10000;
         while (zp>-1)
@@ -15,7 +15,7 @@ public class task4
             System.out.println("Цикл while.Зарплата в " + month + " месяце равна " + zp);
             zp=zp-1000;
         }
-        System.out.println("Цикл do...while");
+        System.out.println("Цикл do...while:");
         zp=10000;
         month=0;
         do
@@ -24,7 +24,7 @@ public class task4
             System.out.println("Цикл do...while.Зарплата в " + month + " месяце равна " + zp);
             zp=zp-1000;
         }while (zp>=0);
-        System.out.println("Цикл for");
+        System.out.println("Цикл for:");
         month=0;
         for (zp = 10000; zp >-1; zp=zp-1000)
         {
---
+++ src/Cycles/task5.java
@@ -0,0 +1,15 @@
+package Cycles;
+
+public class task5
+    /*Напишите программу, которая выводит на экран «5,4,3,2,1» используя цикл for*/
+{
+    public static void main (String[] args)
+    {
+        int i;
+        for (i = 5; i >1; i--)
+        {
+            System.out.print(i + ", ");
+        }
+        System.out.print(i);
+    }
+}
